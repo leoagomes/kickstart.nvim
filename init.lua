d51f47200ef94736c8c58c4ed9d69f7fc30120c2
Leonardo G <leo@tremendous.com>
2025-02-17 22:43:28 -0300

    some themes

--- init.lua
+++ init.lua
@@ -684,24 +684,6 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      -- vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like:
-      -- vim.cmd.hi 'Comment gui=none'
-    end,
-  },
-
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
@@ -800,6 +782,7 @@ require('lazy').setup({
     end,
   },
 
+  -- color schemes
   {
     'Luxed/ayu-vim',
     config = function()
@@ -809,6 +792,22 @@ require('lazy').setup({
       vim.g.ayu_extended_palette = 1
     end,
   },
+  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
+  {
+    'folke/tokyonight.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      -- vim.cmd.colorscheme 'tokyonight-night'
+
+      -- You can configure highlights by doing something like:
+      -- vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+  { 'rebelot/kanagawa.nvim', name = 'kanagawa', priority = 1000 },
+  { 'Mofiqul/vscode.nvim', name = 'vscode', priority = 1000 },
 
   'leafo/moonscript-vim',
 
