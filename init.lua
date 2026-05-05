ccaf48ecfd5c4c51c93a3c9cdb55d9eeb8fab86f
Leonardo G. <lgomes.leo@gmail.com>
2026-05-04 23:03:08 -0300

    add small terminal, color schemes

--- init.lua
+++ init.lua
@@ -964,7 +964,7 @@ do
   -- require 'kickstart.plugins.indent_line'
   -- require 'kickstart.plugins.lint'
   -- require 'kickstart.plugins.autopairs'
-  -- require 'kickstart.plugins.neo-tree'
+  require 'kickstart.plugins.neo-tree'
   -- require 'kickstart.plugins.gitsigns' -- adds gitsigns recommended keymaps
 
   -- NOTE: You can add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
@@ -972,6 +972,20 @@ do
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   -- require 'custom.plugins'
 end
+
+-- ============================================================
+-- Custom themes
+-- ============================================================
+do
+  vim.pack.add(
+    "https://github.com/catppuccin/nvim",
+    "https://github.com/ayu-theme/ayu-vim"
+  )
+end
+
+
+-- add small terminal
+vim.keymap.set("n", "<leader>st", "<CMD>split | resize 10 | term<CR>", { desc = "[S]mall [T]erminal" })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
